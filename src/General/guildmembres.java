07d180c446e28323a28a83338e7c386450897332
Code2Consumer <aboukhriss.zakariahotmail.com>
2015-02-15 19:20:38 +0100

    zfeib

--- src/General/guildmembres.java
+++ src/General/guildmembres.java
@@ -6,10 +6,35 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class guildmembres {
-	public int id, userid, Guildid;
-	public String username;
+	private int id, userid, Guildid;
+	private String username;
 	
 	
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getGuildid() {
+		return Guildid;
+	}
+	public void setGuildid(int guildid) {
+		Guildid = guildid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 	public void getallguildmembresbyguildid(int id){
 		
@@ -18,17 +43,20 @@ public class guildmembres {
 		unebdd.seConnecter();
 		Connection con = unebdd.getMaConnection();
 		
-        String sql =  "select * from utilisateur" ;
+        String sql =  "select * from guildmembres where id = '"+id+"'" ;
         
         try {
 	        Statement smt = con.createStatement() ;
 	        ResultSet rs = smt.executeQuery(sql) ;
 	          while (rs.next()) {
-	              System.out.println(rs.getString("mess") ) ;
+	        	  this.setId(Integer.parseInt(rs.getString("id")));
+	        	  this.setUserid((Integer.parseInt(rs.getString("userid"))));
+	        	  this.setGuildid(Integer.parseInt(rs.getString("Guildid")));
+	        	  this.setUsername( rs.getString("username"));
 	           }
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("un truk a pas marcher ");
+			System.out.println(" Impossible de recuperer ce membre. ");
 		}
         
 	
