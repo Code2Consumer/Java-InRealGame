32f68ac1a9627699be9ce4a091365b51f842b202
Code2Consumer <aboukhriss.zakariahotmail.com>
2015-02-23 17:18:43 +0100

    ihfjsdfibsdfhbjkbjsjhbfguir

--- src/General/guildpost.java
+++ src/General/guildpost.java
@@ -1,6 +1,11 @@
 package General;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 
 public class guildpost {
 	private int id, GuildId, UserId;
@@ -44,7 +49,35 @@ public class guildpost {
 		this.date = date;
 	}
 	
-	
+	public void getallpostforguild(int id){
+		
+
+		BDD unebdd = new BDD();
+		unebdd.chargerPilote();
+		unebdd.seConnecter();
+		Connection con = unebdd.getMaConnection();
+		
+        String sql =  " select * from guildpost where GuildId = "+id ;
+        
+        try {
+	        Statement smt = con.createStatement() ;
+	        ResultSet rs = smt.executeQuery(sql) ;
+	        LinkedList<guildpost> touslesposts = new LinkedList<guildpost>();
+	        
+//	        ecrire un for instead of while 
+//	          while (rs.next()) {
+//	        	  touslesposts.get(i)
+//	        	  this.setId(Integer.parseInt(rs.getString("id")));
+//	        	  this.setUserid((Integer.parseInt(rs.getString("userid"))));
+//	        	  this.setGuildid(Integer.parseInt(rs.getString("Guildid")));
+//	        	  this.setUsername( rs.getString("username"));
+//	           }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(" Impossible de recuperer ce membre. ");
+		}
+		
+	}
 	
 
 }
